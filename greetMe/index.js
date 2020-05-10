5d683acec5f942fe7689ba492ff5f8af7256b053
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-05-10 16:24:24 +0900

    3

--- greetMe/index.js
+++ greetMe/index.js
@@ -1,13 +1,28 @@
-exports.handler = async (event, context) => {
-	context.getRemainingTimeInMillis()
+let messages = [
+	'Hello World!',
+	'Hello Serverless!',
+	"It's a great day today!",
+	"Yay, I'm learning something new today!",
+	'On cloud nine!',
+	'Over the moon!',
+	'Shooting for the stars!',
+	'On top of the World!',
+	'World at my feet!',
+	'Doing everything I love!'
+];
 
-	context.functionName
-	context.functionVersion
-	context.functionArn
-	context.awsRequestId
-	context.memoryLimitInMB
-	context.identity
-	context.logGroupName
-	context.logStreamName
-	context.clientContext.env.platform_version
-}
+exports.handler = async (event) => {
+	// TODO implement
+	let message = messages[Math.floor(Math.random() * 10)];
+	console.error('An error occurred');
+	console.log('A log message ');
+	console.info('An informative message');
+	console.warn('Warning message');
+
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify(message)
+	};
+	throw new Error('This is a random error!');
+	return response;
+};
